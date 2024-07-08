f93712b51bbaf9baf92c70b92c89ada277b871a5
iecube-zhangxiaolong <xiaolong.zhang@iecube.com.cn>
2024-07-08 18:25:29 +0800

    init project

---
+++ src/test/java/com/iecube/ota/OtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iecube.ota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
